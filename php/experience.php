b429aeb5e7c1b061ca419aa6979ad93bdec6fbd2
Amaurydivaystbomer <amaury.divay@sts-sio-caen.info>
2023-12-21 11:55:16 +0100

    amelioration

--- php/experience.php
+++ php/experience.php
@@ -7,3 +7,5 @@ $data = Yaml::parseFile('./../Yaml/experiences.yaml');
 $experience = $data['Experiences'];
 
 echo '<h1>' .$data['Experiences']['title'] . '</h1>';
+echo '<ul>' .$data['Experiences']['titre2'] . '</ul>';
+echo '<li>' .$data['Experiences']['Liste1'] . '</li>';
