f406f85c7a22eadd84d53bffd56be2e63b27831e
Amaurydivaystbomer <amaury.divay@sts-sio-caen.info>
2023-12-21 10:43:18 +0100

    test

--- php/competences.php
+++ php/competences.php
@@ -8,12 +8,12 @@ $experience = $data['Content'];
 
 echo '<h1>' .$data['Content']['title'] . '</h1>';
 echo '<h2>' .$data['Content']['titredev'] . '</h2>';
-echo '<h5>' . $data['Content']['HTML'] . '</h5><img src="' . $data['Content']['imageetoile'] . '" class="etoile">';
+echo '<h5>' . $data['Content']['HTML'] . '</h5><img src="' . $data['Content']['imageetoile'] . '" class="etoile" ,class="titreaug">';
 echo '<h5>' .$data['Content']['CSS'] . '</h5><img src="' .$data['Content']['imageetoile'] .'"class="etoile">';
-echo '<h5>' .$data['Content']['JS'] . '</h5>';
-echo '<h5>' .$data['Content']['PHP'] . '</h5>';
-echo '<h5>' .$data['Content']['PYTHON'] . '</h5>';
+echo '<h5>' .$data['Content']['JS'] . '</h5><img src="' .$data['Content']['imageetoile2'] .'"class="etoile2">';
+echo '<h5>' .$data['Content']['PHP'] . '</h5><img src="' .$data['Content']['imageetoile2'] .'"class="etoile2">';
+echo '<h5>' .$data['Content']['PYTHON'] . '</h5><img src="' .$data['Content']['imageetoile2'] .'"class="etoile2">';
 echo '<p>' .$data['Content']['reseau'] . '</p><img src="' .$data['Content']['imageetoile'] .'"class="etoile">';
-echo '<h2>' .$data['Content']['Langues'] . '</h2><img src="' .$data['Content']['imageetoile2'] .'"class="etoile2">';
+echo '<h2>' .$data['Content']['Langues'] . '</h2>';
 echo '<p>' .$data['Content']['anglais'] . '</p><img src="' .$data['Content']['imageetoile'] .'"class="etoile">';
 echo '<p>' .$data['Content']['espagnol'] . '</p><img src="' .$data['Content']['imageetoile'] .'"class="etoile">';
